f9c9215b35df30ada91868641c4a327ef89d6180
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-07-27 07:58:40 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,24 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
 	private Department entity;
+	private DepartmentService service;
 	
 	@FXML
 	private TextField txtId;
@@ -30,14 +37,39 @@ public class DepartmentFormController implements Initializable{
 		this.entity = entity;
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
+	}
+
 	@FXML
-	public void onBtnSaveAction() {
-		System.out.println("onBtnSaveAction");
+	public void onBtnSaveAction(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		}catch (DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
 	
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tyrParseToInt(txtId.getText()));
+		obj.setName(txtName.getText());
+
+		return obj;
+	}
+
 	@FXML
-	public void onBtnCancelAction() {
-		System.out.println("onBtnCancelAction");
+	public void onBtnCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 	
 	@Override
