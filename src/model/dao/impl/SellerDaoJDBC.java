c85aed210d098fe479dc2021c4cd958e67c01900
Samuel Ghellere <samuel.ghellere@gmail.com>
2021-07-29 17:40:42 -0300

    TextField & DatePicker

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -112,7 +112,7 @@ public class SellerDaoJDBC implements SellerDao{
 			rs = st.executeQuery();
 			if(rs.next()) {
 				Department dep = instantiateDepartment(rs);
-				Seller obj = instantiateDepartment(rs, dep);				
+				Seller obj = instantiateSeller(rs, dep);				
 				return obj;
 			}
 			return null;
@@ -124,13 +124,14 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 	}
 
-	private Seller instantiateDepartment(ResultSet rs, Department dep) throws SQLException {
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
 		Seller obj = new Seller();
 		obj.setId(rs.getInt("Id"));
 		obj.setName(rs.getString("Name"));
 		obj.setEmail(rs.getString("Email"));
 		obj.setBaseSalary(rs.getDouble("BaseSalary"));
-		obj.setBirthDate(rs.getDate("BirthDate"));
+		//obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setBirthDate(new java.util.Date(rs.getTimestamp("BirthDate").getTime()));
 		obj.setDepartment(dep);
 		return obj;
 	}
@@ -165,7 +166,7 @@ public class SellerDaoJDBC implements SellerDao{
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
 
-				Seller obj = instantiateDepartment(rs, dep);
+				Seller obj = instantiateSeller(rs, dep);
 				list.add(obj);
 			}
 			return list;
@@ -201,7 +202,7 @@ public class SellerDaoJDBC implements SellerDao{
 					map.put(rs.getInt("DepartmentId"), dep);
 				}
 
-				Seller obj = instantiateDepartment(rs, dep);
+				Seller obj = instantiateSeller(rs, dep);
 				list.add(obj);
 			}
 			return list;
